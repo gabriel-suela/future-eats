05a603c2e268128f6ddcda72cc6e36068dbdf66e
gabriel-suela <gscsuela@gmail.com>
2023-02-23 11:24:55 -0300

    fix: structured cart info page

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,41 +1,72 @@
 import React, { useContext, useEffect, useState } from "react";
 import { BASE_URL } from "../../constants/url";
 import useProtectedPage from "../../hooks/useProtectedPage";
-import { CartConfig, CartInform, InfoProfile, Main, MainCart } from "./styled";
+import {
+  ButtonStyled,
+  CartConfig,
+  CartInform,
+  Container,
+  InfoProfile,
+  PaymentInfo,
+} from "./styled";
 
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import { useGlobal } from "../../context/GlobalContext";
+import { useRequestData } from "../../hooks/useRequestData";
 
 const Cart = () => {
   useProtectedPage();
 
+  const [data, isLoading] = useRequestData(
+    `${BASE_URL}/profile`,
+    localStorage.getItem("token")
+  );
+
+  const { requests, states } = useGlobal();
+  const { AddToCart, removeToCart } = requests;
+  const { cart } = states;
+
+  const [paymentMethod, setPaymentMethod] = useState({
+    money: false,
+    creditcard: false,
+  });
+
+
+
   return (
-    <Main>
+    <Container>
       <Header visibleArrow={true} title={"Meu Carrinho"}></Header>
       <CartConfig>
         <InfoProfile>
-          <p>Endereço de entrega</p>
-          <p></p>
+          <span>Endereço de entrega</span>
+          <p>{data && data.user.address}</p>
         </InfoProfile>
-        <CartInform></CartInform>
-        <div>
+        <CartInform>
+          <p>carrinho vazio</p>
+        </CartInform>
+        <PaymentInfo>
           <p>Frete R$00,00</p>
           <div>
-            <p>Subtotal</p>
+            <p>SUBTOTAL</p>
             <p>R$00,00</p>
           </div>
-        </div>
-        <h1>Forma de pagamento</h1>
+        </PaymentInfo>
+        <h4>Forma de pagamento</h4>
         <form>
-          <label>Dinheiro</label>
-          <input type={"checkbox"} />
-          <label>Cartão de crédito</label>
-          <input type={"checkbox"} />
-          <button>Confirmar</button>
+          <div>
+            <input id={"money"} name={"money"} type={"radio"} value={paymentMethod}/>
+            <label htmlFor="dinheiro">Dinheiro</label>
+          </div>
+          <div>
+            <input id={"creditcard"} name={"creditcard"} type={"radio"} value={paymentMethod} />
+            <label htmlFor="cartão">Cartão</label>
+          </div>
         </form>
+        <ButtonStyled>Confirmar</ButtonStyled>
       </CartConfig>
       <Footer page={"cart"}></Footer>
-    </Main>
+    </Container>
   );
 };
 
--- src/pages/CartPage/styled.jsx
+++ src/pages/CartPage/styled.jsx
@@ -1,10 +1,34 @@
 import styled from "styled-components";
+import { Button } from "@mui/material";
 
-export const Main = styled.div`
+export const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     flex-direction: column;
+
+    h4 {
+        margin-top: 10px;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        width: 100%;
+        padding: 15px;
+        input {
+      
+            margin: 10px 0px;
+            align-items: center;
+            justify-content: center;
+        }
+
+        label{
+            margin-left: 5px;
+            
+        }
+    }
 `
 
 export const MainCart = styled.div`
@@ -29,23 +53,60 @@ export const InfoProfile = styled.div`
     height: 76px;
     margin-top: 5px;
     flex-grow: 0.1;
-    background-color: lightgrey;
+    background-color: #eee;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
+    padding: 15px;
     justify-content: center;
     font-size: 1rem;
+
+    span {
+        color: var(--greyish);
+        margin-bottom: 10px;
+    }
 `
 
 export const CartInform = styled.div `
     width: 100%;
-    height: 42px;
     display: flex;
     align-items: center;
     justify-content: center;
+    height: 100px;
 `
 
 export const PaymentValue = styled.div `
     width: 100%;
 
 `
+
+
+export const PaymentInfo = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    padding: 15px;
+    
+
+    div{
+        margin-top: 10px;
+        display: flex;
+        justify-content: space-between;
+
+        p {
+            color: var(--mid-green);
+        }
+    }
+`
+
+export const ButtonStyled = styled(Button)`
+  && {
+    margin-top: 16px;
+    height: 42px;
+    padding: 12px 16px;
+    border-radius: 2px;
+    color: black;
+    background-color: #5cb646;
+    width: 90%;
+  }
+`;
