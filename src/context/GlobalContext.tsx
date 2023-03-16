8fcf7a498197b0b4421663e925909a63f8658657
gabriel-suela <gscsuela@gmail.com>
2023-03-16 14:13:57 -0300

    refact: global state

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,5 +1,14 @@
 import { createContext, useContext } from "react";
 
-export const GlobalContext = createContext();
+export const GlobalContext = createContext({
+  states: {
+    restaurant: {}, // Set your default values for restaurant and cart if needed
+    cart: [],
+  },
+  setters: {
+    setRestaurant: {},
+    setCart: {},
+  },
+});
 
 export const useGlobal = () => useContext(GlobalContext);
--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,5 +1,22 @@
-import { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { BASE_URL } from "../utils/url";
 import { GlobalContext } from "./GlobalContext";
+
+interface Restaurant {
+  img: string;
+  name: string;
+  deliveryTime: number;
+  shipping: number;
+}
+
+interface CartItem {}
+
+interface GlobalStateObject {
+  restaurant: Restaurant;
+  cart: CartItem[];
+}
 
 type ChildrenProps = {
   children: React.ReactNode;
@@ -8,6 +25,25 @@ type ChildrenProps = {
 const GlobalState = ({ children }: ChildrenProps) => {
   const [restaurant, setRestaurant] = useState({});
   const [cart, setCart] = useState([]);
+  const { restaurantId } = useParams();
+
+  const fetchRestaurant = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/restaurants`, {
+        headers: {
+          auth: localStorage.getItem("token"),
+        },
+      });
+      setRestaurant(response.data.restaurant);
+      console.log(restaurant);
+    } catch (err) {
+      console.error("An error occurred while trying to get restaurants");
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurant();
+  }, []);
 
   const states = { cart, restaurant };
   const setters = { setRestaurant, setCart };
