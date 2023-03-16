556f994a0213cb0ff13bd268936f24cad16b4efd
gabriel-suela <gscsuela@gmail.com>
2023-03-16 14:48:53 -0300

    feat: restaurant card on feed page

---
+++ src/components/RestaurantCard/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { goToRestaurants } from "../../routes/coordinator";
+import { BoxInformTimePrice, Container } from "./style";
+
+type Restaurant = {
+  id: string;
+  logoUrl: string;
+  shipping: number;
+  img: string;
+  name: string;
+  deliveryTime: number;
+  restaurant: React.ReactNode;
+};
+
+type RestaurantCardProps = {
+  restaurant: Restaurant;
+};
+
+const RestaurantCard = ({ restaurant }: RestaurantCardProps) => {
+  const navigate = useNavigate();
+  return (
+    <Container onClick={() => goToRestaurants(navigate, restaurant.id)}>
+      <img src={restaurant.logoUrl} />
+      <h3>{restaurant.name}</h3>
+      <BoxInformTimePrice>
+        <p>{restaurant.deliveryTime} min</p>
+        <p>
+          Frete R$ {restaurant.shipping.toFixed(2).toString().replace(".", ",")}
+        </p>
+      </BoxInformTimePrice>
+    </Container>
+  );
+};
+
+export default RestaurantCard;
