3c0fe1c59fb7cd4547abd4d985aef627467a47fc
gabriel-suela <gscsuela@gmail.com>
2023-03-16 01:21:26 -0300

    fix: GlobalState types

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import { GlobalContext } from "./GlobalContext";
 
-const GlobalState = ({ children }) => {
+type ChildrenProps = {
+  children: React.ReactNode;
+};
+
+const GlobalState = ({ children }: ChildrenProps) => {
   const [restaurant, setRestaurant] = useState({});
   const [cart, setCart] = useState([]);
 
-
-  const states = {cart, restaurant}
-  const setters = {setRestaurant, setCart}
+  const states = { cart, restaurant };
+  const setters = { setRestaurant, setCart };
   return (
-    <GlobalContext.Provider value={{states, setters}}
-    {children}
+    <GlobalContext.Provider value={{ states, setters }}>
+      {children}
     </GlobalContext.Provider>
-  )
+  );
 };
