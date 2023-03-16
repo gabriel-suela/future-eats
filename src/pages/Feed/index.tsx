442b5da577bd457b5d80b7b230b07a370ab7f6da
gabriel-suela <gscsuela@gmail.com>
2023-03-16 14:49:02 -0300

    feat: feedpage

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,5 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import RestaurantCard from "../../components/RestaurantCard";
+import { useGlobal } from "../../context/GlobalContext";
+import { BASE_URL } from "../../utils/url";
+import { Container } from "./style";
+
+interface RestaurantProps {
+  img: string;
+  name: string;
+  deliveryTime: number;
+  shipping: number;
+}
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [restaurants, setRestaurants] = useState([]);
+
+  const fetchRestaurant = async () => {
+    try {
+      const response = await axios.get<RestaurantProps[]>(
+        `${BASE_URL}/restaurants`,
+        {
+          headers: {
+            auth: localStorage.getItem("token"),
+          },
+        }
+      );
+      setRestaurants(response.data.restaurants);
+      console.log(restaurants);
+    } catch (err) {
+      console.error("An error occurred while trying to get restaurants");
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurant();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Header visibleArrow={true} title={"Restaurantes"} />
+        {restaurants.length > 0 &&
+          restaurants.map((restaurant, item) => {
+            return <RestaurantCard key={item} restaurant={restaurant} />;
+          })}
+      </Container>
+    </>
+  );
 };
 
 export default Feed;
