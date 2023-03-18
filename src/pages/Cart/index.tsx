bb1524e0410adbc549060c215008edd954691c0a
gabriel-suela <gscsuela@gmail.com>
2023-03-17 21:36:37 -0300

    feat: cart component done

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,118 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CardProduct from "../../components/CardProduct";
+import Header from "../../components/Header";
+import NavBar from "../../components/NavBar";
+import { useGlobal } from "../../context/GlobalContext";
+import useProtectedPage from "../../hooks/useProtectedPage";
+import { BASE_URL } from "../../utils/url";
+import { ApiResponse, ProfileProps } from "../Profile";
+import {
+  ButtonStyled,
+  CartConfig,
+  CartInform,
+  Container,
+  InfoProfile,
+  InfoRestaurant,
+  PaymentInfo,
+} from "./styled";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const [profile, setProfile] = useState<ProfileProps | null>(null);
+  const [payment, setPayment] = useState<string>("");
+  const [fullPrice, setFullPrice] = useState(() => 0);
+  const { states, setters } = useGlobal();
+  const { cart = [], restaurant } = states;
+  const [paymentMethod] = useState<Array<string>>([
+    "Dinheiro",
+    "Cartão de Crédito",
+  ]);
+  useProtectedPage();
+
+  const fetchProfile = async () => {
+    try {
+      const response = await axios.get<ApiResponse>(`${BASE_URL}/profile`, {
+        headers: {
+          auth: localStorage.getItem("token"),
+        },
+      });
+      setProfile(response.data.user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProfile();
+    totalPrice();
+  }, []);
+
+  const totalPrice = () => {
+    let totalPrice = 0;
+    for (const product of cart ?? []) {
+      totalPrice += product.price * product.quantity;
+    }
+    setFullPrice(totalPrice);
+  };
+
+  const onChangePayment = (e: any) => {
+    setPayment(e.target.value);
+  };
+
+  return (
+    <Container>
+      <Header visibleArrow={true} title={"Meu Carrinho"} />
+      <CartConfig>
+        <InfoProfile>
+          <span>Endereço de entrega</span>
+          <p>{profile && profile.address}</p>
+        </InfoProfile>
+        <InfoRestaurant>
+          <h4>{restaurant?.name}</h4>
+          <p>{restaurant?.address}</p>
+          <p>30 - {restaurant?.deliveryTime} min</p>
+        </InfoRestaurant>
+        <CartInform>
+          {cart?.length ?? 0 > 0 ? (
+            cart?.map((product) => {
+              return <CardProduct product={product} restaurant={restaurant} />;
+            })
+          ) : (
+            <p>Carrinho Vazio</p>
+          )}
+        </CartInform>
+        <PaymentInfo>
+          <p>Frete R$ {restaurant?.shipping},00</p>
+          <div>
+            <p>SUBTOTAL</p>
+            <p>R$ {fullPrice.toFixed(2).toString().replace(".", ",")}</p>
+          </div>
+        </PaymentInfo>
+        <h4>Forma de pagamento</h4>
+        <form>
+          {paymentMethod.map((element, index) => {
+            const checked = payment === element;
+            return (
+              <div key={index}>
+                <input
+                  checked={checked}
+                  name={"paymentMethod"}
+                  id={index.toString()}
+                  type={"radio"}
+                  onChange={onChangePayment}
+                  value={element}
+                ></input>
+                <label>{element}</label>
+              </div>
+            );
+          })}
+          <ButtonStyled type="submit">Confirmar</ButtonStyled>
+        </form>
+      </CartConfig>
+
+      <NavBar page="cart" />
+    </Container>
+  );
 };
 
 export default Cart;
---
+++ src/pages/Cart/styled.tsx
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  h4 {
+    margin-top: 10px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 15px;
+    input {
+      margin: 10px 0px;
+      align-items: center;
+      justify-content: center;
+    }
+
+    label {
+      margin-left: 5px;
+    }
+  }
+`;
+
+export const MainCart = styled.div`
+  font-size: 1rem;
+  height: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CartConfig = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  /* justify-content: space-around; */
+`;
+
+export const InfoProfile = styled.div`
+  width: 100%;
+  height: 76px;
+  margin-top: 5px;
+  flex-grow: 0.1;
+  background-color: #eee;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 15px;
+  justify-content: center;
+  font-size: 1rem;
+
+  span {
+    color: var(--greyish);
+    margin-bottom: 10px;
+  }
+`;
+
+export const CartInform = styled.div`
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PaymentValue = styled.div`
+  width: 100%;
+`;
+
+export const PaymentInfo = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding: 15px;
+
+  div {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+
+    p {
+      color: var(--mid-green);
+    }
+  }
+`;
+
+export const ButtonStyled = styled.button`
+  margin-top: 16px;
+  height: 42px;
+  border-radius: 2px;
+  color: black;
+  background-color: var(--mid-green);
+  width: 100%;
+`;
+
+export const InfoRestaurant = styled.div`
+  width: 100%;
+  padding-left: 15px;
+  margin: 5px 0px;
+  h4 {
+    color: var(--mid-green);
+  }
+  p {
+    color: var(--greyish);
+  }
+`;
