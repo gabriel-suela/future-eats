a7883d307f49b8c186c6488bbd0c33a1f1adb1c3
gabriel-suela <gscsuela@gmail.com>
2023-03-20 00:53:44 -0300

    feat: logout button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,35 @@
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
-import { goToLastPage } from "../../routes/coordinator";
+import { goToLastPage, goToLogin } from "../../routes/coordinator";
 import { Container, Title } from "./style";
+import { CiLogout } from "react-icons/ci";
 
 interface HeaderProps {
-  title?: string;
-  visibleArrow: boolean;
+	title?: string;
+	visibleArrow: boolean;
 }
 
 const Header = ({ title, visibleArrow }: HeaderProps) => {
-  const navigate = useNavigate();
-  return (
-    <Container>
-      {visibleArrow && (
-        <ChevronLeftIcon
-          boxSize={7}
-          onClick={() => {
-            goToLastPage(navigate);
-          }}
-        />
-      )}
-      <Title>{title}</Title>
-    </Container>
-  );
+	const logOut = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+
+	const navigate = useNavigate();
+	return (
+		<Container>
+			{visibleArrow && (
+				<ChevronLeftIcon
+					boxSize={7}
+					onClick={() => {
+						goToLastPage(navigate);
+					}}
+				/>
+			)}
+			<Title>{title}</Title>
+			<CiLogout onClick={logOut} />
+		</Container>
+	);
 };
 
 export default Header;
