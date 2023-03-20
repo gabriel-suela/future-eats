d6c0deb75029185919a7774f87952b032b6cf785
gabriel-suela <gscsuela@gmail.com>
2023-03-19 23:17:01 -0300

    chore: remote import react from react

--- src/components/OrderHistory/index.tsx
+++ src/components/OrderHistory/index.tsx
@@ -1,64 +1,64 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "../../utils/url";
 import { OrderCard } from "./styled";
 
 interface OrderProps {
-  totalPrice: number;
-  createdAt: number;
-  id: string;
-  restaurantName: string;
+	totalPrice: number;
+	createdAt: number;
+	id: string;
+	restaurantName: string;
 }
 
 const OrderHistory = () => {
-  const [orderHistory, setOrderHistory] = useState<OrderProps[]>([]);
+	const [orderHistory, setOrderHistory] = useState<OrderProps[]>([]);
 
-  const fetchOrderHistory = async () => {
-    try {
-      const response = await axios.get(`${BASE_URL}/orders/history`, {
-        headers: {
-          auth: localStorage.getItem("token"),
-        },
-      });
-      setOrderHistory(response.data.orders);
-      console.log(orderHistory);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	const fetchOrderHistory = async () => {
+		try {
+			const response = await axios.get(`${BASE_URL}/orders/history`, {
+				headers: {
+					auth: localStorage.getItem("token"),
+				},
+			});
+			setOrderHistory(response.data.orders);
+			console.log(orderHistory);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    fetchOrderHistory();
-  }, []);
+	useEffect(() => {
+		fetchOrderHistory();
+	}, []);
 
-  const listHistory =
-    orderHistory &&
-    orderHistory.map((order) => {
-      let subtotal = order.totalPrice.toFixed(2).toString().replace(".", ",");
-      let newTime = new Date(order.createdAt);
+	const listHistory =
+		orderHistory &&
+		orderHistory.map((order) => {
+			let subtotal = order.totalPrice.toFixed(2).toString().replace(".", ",");
+			let newTime = new Date(order.createdAt);
 
-      const formattedDate = newTime.toLocaleDateString("pt-br", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-      });
-      return (
-        <OrderCard key={order.id}>
-          <p>{order.restaurantName}</p>
-          <p>{formattedDate}</p>
-          <p>SUBTOTAL R${subtotal}</p>
-        </OrderCard>
-      );
-    });
+			const formattedDate = newTime.toLocaleDateString("pt-br", {
+				day: "numeric",
+				month: "long",
+				year: "numeric",
+			});
+			return (
+				<OrderCard key={order.id}>
+					<p>{order.restaurantName}</p>
+					<p>{formattedDate}</p>
+					<p>SUBTOTAL R${subtotal}</p>
+				</OrderCard>
+			);
+		});
 
-  return (
-    <>
-      {orderHistory && orderHistory.length > 0 && listHistory}
-      {orderHistory && orderHistory.length === 0 && (
-        <p>Você ainda não realizou nenhum pedido</p>
-      )}
-    </>
-  );
+	return (
+		<>
+			{orderHistory && orderHistory.length > 0 && listHistory}
+			{orderHistory && orderHistory.length === 0 && (
+				<p>Você ainda não realizou nenhum pedido</p>
+			)}
+		</>
+	);
 };
 
 export default OrderHistory;
